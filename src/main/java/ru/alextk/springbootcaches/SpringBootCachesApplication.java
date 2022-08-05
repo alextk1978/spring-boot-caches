9fdfb2bb4778f1d3c546bd77173980bdf7621b74
Aleksey Tkachenko <a.tkachenko@platformaofd.ru>
2022-08-05 16:56:00 +0300

    Initial commit

---
+++ src/main/java/ru/alextk/springbootcaches/SpringBootCachesApplication.java
@@ -0,0 +1,13 @@
+package ru.alextk.springbootcaches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCachesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCachesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/alextk/springbootcaches/SpringBootCachesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alextk.springbootcaches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCachesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
